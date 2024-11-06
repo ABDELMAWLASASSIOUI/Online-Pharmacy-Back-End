27d3d2253d84c95eeffea2ed4a7de6266ee91ef1
ABDELMAWLA SASSIOUI <Sassioui.Abdelmawla@ump.ac.ma>
2024-11-06 12:26:47 +0100

    add pepeline helf

--- src/main/java/com/pharmacy_online_platforme/controllers/CategorieController.java
+++ src/main/java/com/pharmacy_online_platforme/controllers/CategorieController.java
@@ -74,4 +74,7 @@ public class CategorieController {
     @GetMapping("/admin/pipeline")
     public String testingJenkinsfinalpipline(){
         return "buils project with jenkins final pipleline";}
+    @GetMapping("/admin/addpipline")
+    public String addpipline(){
+        return "add pipline";}
 }
